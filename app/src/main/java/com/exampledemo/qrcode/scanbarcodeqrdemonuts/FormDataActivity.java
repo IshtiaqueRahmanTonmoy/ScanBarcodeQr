26756cb3b699aa148185c0b3223608ffbfddb6f5
TONMOYPC <ruhan.ahmedrh@gmail.com>
2017-06-28 01:51:24 +0600

    new commit

--- app/src/main/java/com/exampledemo/qrcode/scanbarcodeqrdemonuts/FormDataActivity.java
+++ app/src/main/java/com/exampledemo/qrcode/scanbarcodeqrdemonuts/FormDataActivity.java
@@ -32,7 +32,7 @@ public class FormDataActivity extends AppCompatActivity {
     private Button btnSave;
     private DatabaseHandler db;
     private String name,result,date,note;
-
+    SharedPreferences mSharedPreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,12 +56,16 @@ public class FormDataActivity extends AppCompatActivity {
         String strDate = mdformat.format(calendar.getTime());
         edtDate.setText(""+strDate);
 
-        final SharedPreferences mSharedPreference= PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-        String value=(mSharedPreference.getString("message", "Default_Value"));
+        mSharedPreference= PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        String value=(mSharedPreference.getString("message", ""));
         //String code =(mSharedPreference.getString("code", "Default_Value"));
 
-        edtResult.setText("" + value);
-
+        if(value!=null) {
+            edtResult.setText("" + value);
+        }
+        else {
+            edtResult.setText("");
+        }
         //Toast.makeText(FormDataActivity.this, ""+value, Toast.LENGTH_SHORT).show();
 
         db = new DatabaseHandler(this);
@@ -81,10 +85,22 @@ public class FormDataActivity extends AppCompatActivity {
                 }else{
 
                     db.insert(name,result,date,note);
+                    mSharedPreference.edit().clear().commit();
+                    edtName.setText("");
+                    edtResult.setText("");
+                    edtDate.setText("");
+                    edtNote.setText("");
                     Toast.makeText(getApplicationContext(),"Successfully added", Toast.LENGTH_SHORT).show();
+
                 }
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
     }
 
     @Override
